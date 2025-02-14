327d70143d5f9a8d80391a7a921c113d4eebda40
Andrei Trubin <atrubin1992@gmail.com>
2025-02-14 09:14:42 -0500

    split the SnakeGame.ts into more functions

--- TypeScript/SnakeGame.ts
+++ TypeScript/SnakeGame.ts
@@ -12,15 +12,23 @@ const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 const boxSize = 20;
 
-let snake: SnakePart[] = [{ x: 10, y: 10 }];
+const fieldWidth = canvasWidth / boxSize;
+const fieldHeight = canvasHeight / boxSize;
+
+let snake: SnakePart[];
 
 let foodX = 15;
 let foodY = 15;
 
 let currentHeadStep = SnakeHead.Step.Right;
 
-placeFood();
+startGame();
 main();
+
+function startGame(){
+  snake = [{ x: 10, y: 10 }];
+  placeFood();
+}
 
 function main(): void {
   update();
@@ -29,45 +37,57 @@ function main(): void {
 }
 
 function update(): void {
-  const [dx, dy] = SnakeHead.getOffsets(currentHeadStep) 
+  const newHead = calcNewHead()
 
-  const head: SnakePart = {
-    x: snake[0].x + dx,
-    y: snake[0].y + dy,
-  };
-
-  if (head.x < 0) {
-    head.x = (canvasWidth / boxSize) - 1;
-  } else if (head.x >= canvasWidth / boxSize) {
-    head.x = 0;
-  }
-
-  if (head.y < 0) {
-    head.y = (canvasHeight / boxSize) - 1;
-  } else if (head.y >= canvasHeight / boxSize) {
-    head.y = 0;
-  }
-
-  if (head.x === foodX && head.y === foodY) {
+  if (newHead.x === foodX && newHead.y === foodY) {
     placeFood();
   } else {
     snake.pop();
   }
 
+  if (isGameOver(newHead)) {
+    startGame();
+  }
+
+  snake.unshift(newHead);
+}
+
+function calcNewHead() {
+  const [dx, dy] = SnakeHead.getOffsets(currentHeadStep) 
+
+  const newHead: SnakePart = {
+    x: snake[0].x + dx,
+    y: snake[0].y + dy,
+  };
+
+  if (newHead.x < 0) {
+    newHead.x = fieldWidth - 1;
+  } else if (newHead.x >= fieldWidth) {
+    newHead.x = 0;
+  }
+
+  if (newHead.y < 0) {
+    newHead.y = fieldHeight - 1;
+  } else if (newHead.y >= fieldHeight) {
+    newHead.y = 0;
+  }
+
+  return newHead
+}
+
+function isGameOver(newHead: SnakePart): boolean {
   for (let i = 0; i < snake.length; i++) {
-    if (snake[i].x === head.x && snake[i].y === head.y) {
-      snake = [{ x: 10, y: 10 }];
-      placeFood();
-      return;
+    if (snake[i].x === newHead.x && snake[i].y === newHead.y) {
+      return true;
     }
   }
 
-  snake.unshift(head);
+  return false;
 }
 
 function placeFood(): void {
-  foodX = Math.floor(Math.random() * (canvasWidth / boxSize));
-  foodY = Math.floor(Math.random() * (canvasHeight / boxSize));
+  foodX = Math.floor(Math.random() * fieldWidth);
+  foodY = Math.floor(Math.random() * fieldHeight);
 }
 
 function draw(): void {

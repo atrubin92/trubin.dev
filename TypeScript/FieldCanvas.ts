d65c3d014e9b52cd1e3abfd0c94a33c4e0c262a8
Andrei Trubin <atrubin1992@gmail.com>
2025-02-22 20:51:24 -0500

    it is possible to adjust the number of steps in width and height

--- TypeScript/FieldCanvas.ts
+++ TypeScript/FieldCanvas.ts
@@ -1,19 +1,44 @@
 import { SnakeCell } from "./SnakeCell";
 
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-const context = canvas.getContext("2d")!;
+const ctx = canvas.getContext("2d")!;
 
-const boxSize = 20;
+export const fieldWidth = 7;
+export const fieldHeight = 5;
 
-export const fieldWidth = canvas.width / boxSize;
-export const fieldHeight = canvas.height / boxSize;
+const boxWidth = canvas.width / fieldWidth;
+const halfBoxWidth = boxWidth / 2;
+
+const boxHeight = canvas.height / fieldHeight;
+const halfBoxHeight = boxHeight / 2;
 
 export function draw(snake: SnakeCell[], food: SnakeCell): void {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "green";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawGrid();
+
+    ctx.fillStyle = "green";
     for (const part of snake) {
-        context.fillRect(part.x * boxSize, part.y * boxSize, boxSize, boxSize);
+        ctx.fillRect(part.x * boxWidth, part.y * boxHeight, boxWidth, boxHeight);
     }
-    context.fillStyle = "red";
-    context.fillRect(food.x * boxSize, food.y * boxSize, boxSize, boxSize);
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x * boxWidth, food.y * boxHeight, boxWidth, boxHeight);
+}
+
+function drawGrid() {
+    for (let cellCenterX = halfBoxWidth; cellCenterX < canvas.width; cellCenterX += boxWidth) {
+        drawLine(cellCenterX, 0, cellCenterX, canvas.height)
+    }
+
+    for (let cellCenterY = halfBoxHeight; cellCenterY < canvas.height; cellCenterY += boxHeight) {
+        drawLine(0, cellCenterY, canvas.width, cellCenterY)
+    }
+}
+
+function drawLine(x1: number, y1: number, x2: number, y2: number) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = 'blue';
+    ctx.stroke();
 }

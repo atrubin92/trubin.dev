422219953a837db7df0f91f085caa515b18f2324
Andrei Trubin <atrubin1992@gmail.com>
2025-03-24 13:35:02 -0400

    refactoring SnakeCellRenderer file

--- TypeScript/renderer/snake-cell/SnakeCellRenderer.ts
+++ TypeScript/renderer/snake-cell/SnakeCellRenderer.ts
@@ -2,5 +2,15 @@ import * as SquareSnakeCellRenderer from "./_SquareSnakeCellRenderer";
 
 export function drawSnakeCell(partX: number, partY: number, snakeCellScale: number) {
     const colorArray: string[] = ["green", "rgb(128, 0, 0)", "blue", "red"]
-    SquareSnakeCellRenderer.drawSnakeCell(partX, partY, colorArray, snakeCellScale)
+    for (let colorIndex = 0; colorIndex < colorArray.length; colorIndex++){
+        const colorItem = colorArray[colorIndex]
+        const colorPartScale = 1 - colorIndex / colorArray.length
+        const layerScale = snakeCellScale * colorPartScale
+
+        drawPartSnakeCell(partX, partY, colorItem, layerScale)
+    }
+}
+
+function drawPartSnakeCell(partX: number, partY: number, color: string, sizePercentage: number){
+    SquareSnakeCellRenderer.drawCell(partX, partY, color, sizePercentage)
 }
--- TypeScript/renderer/snake-cell/_SquareSnakeCellRenderer.ts
+++ TypeScript/renderer/snake-cell/_SquareSnakeCellRenderer.ts
@@ -1,17 +1,7 @@
 import { getBoxWidth, getBoxHeight, getHalfBoxWidth, getHalfBoxHeight } from "../../settings/Settings";
 import { context } from "../_CanvasContext";
 
-export function drawSnakeCell(partX: number, partY: number, colorArray: string[], snakeCellScale: number) {
-    for (let colorIndex = 0; colorIndex < colorArray.length; colorIndex++){
-        const colorItem = colorArray[colorIndex]
-        const colorPartScale = 1 - colorIndex / colorArray.length
-        const layerScale = snakeCellScale * colorPartScale
-
-        drawCell(partX, partY, colorItem, layerScale)
-    }
-}
-
-function drawCell(partX: number, partY: number, color: string, sizePercentage: number = 1) {
+export function drawCell(partX: number, partY: number, color: string, sizePercentage: number) {
     context.fillStyle = color
     context.fillRect(
         partX * getBoxWidth() + getHalfBoxWidth() * (1 - sizePercentage),

5904b5d638c9a2d1d768ad37d4d751b6ab3b2b93
Andrei Trubin <atrubin1992@gmail.com>
2025-03-24 11:56:47 -0400

    SnakeCell can contain many colors

--- TypeScript/renderer/snake-cell/SnakeCellRenderer.ts
+++ TypeScript/renderer/snake-cell/SnakeCellRenderer.ts
@@ -1,5 +1,6 @@
 import * as SquareSnakeCellRenderer from "./_SquareSnakeCellRenderer";
 
 export function drawSnakeCell(partX: number, partY: number, snakeCellScale: number) {
-    SquareSnakeCellRenderer.drawSnakeCell(partX, partY, snakeCellScale)
+    const colorArray: string[] = ["green", "rgb(128, 0, 0)", "blue", "red"]
+    SquareSnakeCellRenderer.drawSnakeCell(partX, partY, colorArray, snakeCellScale)
 }
--- TypeScript/renderer/snake-cell/_SquareSnakeCellRenderer.ts
+++ TypeScript/renderer/snake-cell/_SquareSnakeCellRenderer.ts
@@ -1,9 +1,14 @@
 import { getBoxWidth, getBoxHeight, getHalfBoxWidth, getHalfBoxHeight } from "../../settings/Settings";
 import { context } from "../_CanvasContext";
 
-export function drawSnakeCell(partX: number, partY: number, snakeCellScale: number) {
-    drawCell(partX, partY, "green", snakeCellScale)
-    drawCell(partX, partY, "rgb(128, 0, 0)", 0.5 * snakeCellScale)
+export function drawSnakeCell(partX: number, partY: number, colorArray: string[], snakeCellScale: number) {
+    for (let colorIndex = 0; colorIndex < colorArray.length; colorIndex++){
+        const colorItem = colorArray[colorIndex]
+        const colorPartScale = 1 - colorIndex / colorArray.length
+        const layerScale = snakeCellScale * colorPartScale
+
+        drawCell(partX, partY, colorItem, layerScale)
+    }
 }
 
 function drawCell(partX: number, partY: number, color: string, sizePercentage: number = 1) {

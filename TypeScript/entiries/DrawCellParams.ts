7fe37a1935cd277ef037684589df0c4878f6e193
Andrei Trubin <atrubin1992@gmail.com>
2025-03-25 10:47:00 -0400

    create DrawCellParams.ts

---
+++ TypeScript/entiries/DrawCellParams.ts
@@ -0,0 +1,39 @@
+export class DrawCellParams {
+    context!: CanvasRenderingContext2D;
+    row!: number;
+    column!: number;
+    color!: string;
+    sizePercentage!: number;
+
+    private _boxWidth!: number;
+    private _boxHeight!: number;
+
+    private _halfBoxWidth!: number;
+    private _halfBoxHeight!: number;
+
+    set boxWidth(value: number) {
+        this._boxWidth = value;
+        this._halfBoxWidth = value / 2;
+    }
+
+    set boxHeight(value: number) {
+        this._boxHeight = value;
+        this._halfBoxHeight = value / 2;
+    }
+
+    get boxWidth() {
+        return this._boxWidth;
+    }
+
+    get boxHeight() {
+        return this._boxHeight;
+    }
+
+    get halfBoxWidth() {
+        return this._halfBoxWidth;
+    }
+
+    get halfBoxHeight() {
+        return this._halfBoxHeight;
+    }
+}

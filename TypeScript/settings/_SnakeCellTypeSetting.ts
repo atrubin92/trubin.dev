58161532c6909171b2ed6e28261b822a1345bc96
Andrei Trubin <atrubin1992@gmail.com>
2025-03-24 23:33:51 -0400

    prevent changing the selected cell type input using keyboard arrows

--- TypeScript/settings/_SnakeCellTypeSetting.ts
+++ TypeScript/settings/_SnakeCellTypeSetting.ts
@@ -9,12 +9,17 @@ let currentSnakeCellType: SnakeCellType = SnakeCellType.SQUARE;
 const radios = document.querySelectorAll<HTMLInputElement>('input[name="cellType"]');
 
 radios.forEach(radio => {
+    radio.addEventListener("keydown", event => {
+        if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)) {
+            event.preventDefault();
+        }
+    });
+
     radio.addEventListener("change", () => {
         const value = radio.value;
 
         if (Object.values(SnakeCellType).includes(value as SnakeCellType)) {
             currentSnakeCellType = value as SnakeCellType;
-            console.log("Selected Cell Type:", currentSnakeCellType);
         }
     });
 });

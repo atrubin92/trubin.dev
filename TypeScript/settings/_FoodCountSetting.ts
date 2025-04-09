b8c565a19fcd0dc786e62d68832ff8ea80e73696
Andrei Trubin <atrubin1992@gmail.com>
2025-04-09 07:09:53 -0400

    foodCount can be zero when the field width and height are one

--- TypeScript/settings/_FoodCountSetting.ts
+++ TypeScript/settings/_FoodCountSetting.ts
@@ -12,11 +12,21 @@ export function updateFoodLimit() {
     foodCountMax = FieldSize.getFieldWidth() * FieldSize.getFieldHeight() - INITIAL_FREE_CELLS
     foodCountInput.max = foodCountMax.toString()
 
+    if (foodCountMax == 0) {
+        foodCountInput.min = "0"
+    } else {
+        foodCountInput.min = "1"
+        if (foodCount == 0) {
+            foodCountInput.value = "1"
+            foodCount = 1
+        }
+    }
+
     const currentFoodCount = parseInt(foodCountInput.value, 10);
     if (isNaN(currentFoodCount) || currentFoodCount > foodCountMax) {
         foodCountInput.value = foodCountMax.toString()
         foodCount = foodCountMax;
-        foodCountInput.style.backgroundColor = "";
+        updateFoodCountBackground()
     }
 }
 
@@ -41,17 +51,20 @@ foodCountInput.addEventListener('keydown', (event) => {
     }
 });
 
-foodCountInput.addEventListener("input", () => {
+foodCountInput.addEventListener("input", () => updateFoodCountBackground());
+
+function updateFoodCountBackground() {
     const value = parseInt(foodCountInput.value, 10);
     if (isValidFoodCount(value)) {
         foodCountInput.style.backgroundColor = "";
     } else {
         foodCountInput.style.backgroundColor = "red";
     }
-});
+}
 
 function isValidFoodCount(value: number): boolean {
-    return !isNaN(value) &&
-        1 <= value &&
-        value < FieldSize.getFieldWidth() * FieldSize.getFieldHeight()
+    if (isNaN(value)) return false
+    if (value == 0 && FieldSize.getFieldWidth() * FieldSize.getFieldHeight() == 1) return true
+    if (1 <= value && value < FieldSize.getFieldWidth() * FieldSize.getFieldHeight()) return true
+    return false
 }

2286b11929e6451f5ed961c354257715f3e6ae68
Andrei Trubin <atrubin1992@gmail.com>
2025-04-21 22:42:18 -0400

    slider will update the canvas size in the future

--- TypeScript/settings/_CanvasSizeSetting.ts
+++ TypeScript/settings/_CanvasSizeSetting.ts
@@ -1,6 +1,9 @@
+import * as BoxSize from "./_BoxSizeSetting"
+
 const slider = document.getElementById('canvasSizeSlider') as HTMLInputElement;
 const sliderValue = document.getElementById('canvasSizeValue') as HTMLElement;
 
 slider.addEventListener('input', () => {
   sliderValue.textContent = slider.value;
+  BoxSize.updateBoxSize()
 });
